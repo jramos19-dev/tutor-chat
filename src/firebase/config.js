ef4488a301dd99f6d32f387f4a1a885ecb317dcd
Jorge Ramos <jramos19@southtexascollege.edu>
2021-07-06 16:11:01 -0500

    added signup and login forms

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC0tBi-d7U8YL7LjBCReVFHEhLBDYIna48",
+    authDomain: "udemyvue-7106c.firebaseapp.com",
+    projectId: "udemyvue-7106c",
+    storageBucket: "udemyvue-7106c.appspot.com",
+    messagingSenderId: "58870491524",
+    appId: "1:58870491524:web:fd4f8f459223815f2f95d1"
+};
+  
+
+//init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export {projectFirestore,timestamp}
