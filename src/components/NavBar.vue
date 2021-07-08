328a266ca0a4eb9f730c230db0d47f2ec452fc30
Jorge Ramos <jramos19@southtexascollege.edu>
2021-07-07 21:33:45 -0500

    added navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+      <div>
+          <p>Hey there ... display name</p>
+            <p class="email">currently logged in as ...email</p>
+          </div>
+        
+        <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+nav {
+    padding: 20px;
+    border-bottom: 1px solid #eee;
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+}
+
+
+
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,9 +1,16 @@
 <template>
-  <p>Chatroom</p>
+<div class="container">
+    <NavBar/>
+</div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue'
 export default {
+    components: {NavBar},
+    setup(){
+
+    }
 
 }
 </script>
