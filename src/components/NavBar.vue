f6c7e336278b97c0b5063782d6d00e0ecd22de36
Jorge Ramos <jramos19@southtexascollege.edu>
2021-07-08 11:30:31 -0500

    logout composable added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,12 +5,27 @@
             <p class="email">currently logged in as ...email</p>
           </div>
         
-        <button>Logout</button>
+        <button @click="handleClick">Logout</button>
   </nav>
 </template>
 
 <script>
+import useLogout from '../composables/useLogout'
 export default {
+ setup(){
+     
+     const {logout, error} = useLogout()
+
+
+    const handleClick = async () =>{
+        await logout()
+        if(!error.value) {
+            console.log('user lgged out')
+        }
+    }
+
+    return {handleClick,error}
+ }
 
 }
 </script>
@@ -22,9 +37,19 @@ nav {
     border-bottom: 1px solid #eee;
     display: flex;
     justify-content: space-between;
-    align-items:center;
+    align-items: center;
 }
 
+nav p {
+    margin: 2px auto;
+    font-size: 16px; 
+    color: #444;
+}
+
+nav p.email {
+    font-size:  14px;
+    color: #999;
+}
 
 
 </style>
