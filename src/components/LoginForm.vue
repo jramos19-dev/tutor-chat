30aaf5d9ad2c2dd9c7e2ded41b7f3c90b0227c14
Jorge Ramos <jramos19@southtexascollege.edu>
2021-07-07 19:37:54 -0500

    error outputting login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -2,23 +2,30 @@
   <form @submit.prevent="handleSubmit"> 
       <input type="email" required placeholder="email" v-model="email">
       <input type="password" required placeholder="password" v-model="password">
+      <div class="error">{{error}}</div>
       <button>Sign up</button>
   </form>
 </template>
 
 <script>
 import { ref } from '@vue/reactivity'
+import useLogin from '../composables/useLogin'
 export default {
 
 setup(){
     const email = ref('')
     const password = ref('')
+    
+    const {error,login} = useLogin()
 
-    const handleSubmit = () =>{
-        console.log(email.value, password.value)
-    }
+    const handleSubmit = async () =>{
+        await login(email.login,password.value)
+        if(!error.value){
+            console.log('user logged in ')
+        }
+}
 
-    return {email,password,handleSubmit}
+    return {email,password,handleSubmit,error}
 }
 }
 </script>
---
+++ src/composables/useLogin.js
@@ -0,0 +1,28 @@
+import { ref } from 'vue'
+import { projectAuth, projectFirestore } from '../firebase/config'
+
+const error = ref(null)
+
+
+const login = async (email, password) => {
+    error.value = null 
+     
+    try {
+        const res = await projectAuth.signInWithEmailAndPassword(email, password)
+        error.value = null
+        console.log(res)
+        return res
+    } catch (err) {
+        console.log(err.value)
+        error.value = 'Incorrect login '
+    }
+}
+
+
+const useLogin = () => {
+    return {error,login}
+}
+
+
+
+export default useLogin 
