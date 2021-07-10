9d433f0fec10778e6a7187b0c4ec50565d7f7c77
jramos19-dev <jramos19@southtexascollege.edu>
2021-07-10 16:45:58 -0500

    added the getUser composable

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,55 +1,51 @@
 <template>
-  <nav>
-      <div>
-          <p>Hey there ... display name</p>
-            <p class="email">currently logged in as ...email</p>
-          </div>
-        
-        <button @click="handleClick">Logout</button>
+  <nav v-if="user">
+    <div>
+      <p>Hey there ... {{ user.displayName }}</p>
+      <p class="email">currently logged in as ...{{ user.email }}</p>
+    </div>
+
+    <button @click="handleClick">Logout</button>
   </nav>
 </template>
 
 <script>
+import getUser from '../composables/getUser'
 import useLogout from '../composables/useLogout'
 export default {
- setup(){
-     
-     const {logout, error} = useLogout()
+  setup() {
+    const { logout, error } = useLogout()
+    const { user } = getUser()
 
-
-    const handleClick = async () =>{
-        await logout()
-        if(!error.value) {
-            console.log('user lgged out')
-        }
+    const handleClick = async () => {
+      await logout()
+      if (!error.value) {
+        console.log('user lgged out')
+      }
     }
 
-    return {handleClick,error}
- }
-
+    return { handleClick, error, user }
+  },
 }
 </script>
 
 <style>
-
 nav {
-    padding: 20px;
-    border-bottom: 1px solid #eee;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 nav p {
-    margin: 2px auto;
-    font-size: 16px; 
-    color: #444;
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
 }
 
 nav p.email {
-    font-size:  14px;
-    color: #999;
+  font-size: 14px;
+  color: #999;
 }
-
-
 </style>
