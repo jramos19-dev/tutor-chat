a8ec4a8ae1609fbcd1ce67db2c553fd98a0c0a92
jramos19-dev <jramos19@southtexascollege.edu>
2021-07-10 17:09:56 -0500

    working logout

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,20 +1,29 @@
 <template>
-<div class="container">
-    <NavBar/>
-</div>
+  <div class="container">
+    <NavBar />
+  </div>
 </template>
 
 <script>
+import getUser from '../composables/getUser'
 import NavBar from '../components/NavBar.vue'
+import { watch } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
 export default {
-    components: {NavBar},
-    setup(){
+  components: { NavBar },
+  setup() {
+    const router = useRouter()
+    const { user } = getUser()
 
-    }
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: 'Welcome' })
+      }
+    })
 
+    return { user }
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
