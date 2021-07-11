2440ee8633276da61f6fc5d9be151e3012dcda57
jramos19-dev <jramos19@southtexascollege.edu>
2021-07-11 13:54:57 -0500

    auto scroll to bottom of the messages when new message

--- src/components/ChatWindow.vue
+++ src/components/ChatWindow.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="chat-window">
     <div v-if="error">{{ error }}</div>
-    <div v-if="documents" class="messages">
-      <div v-for="doc in documents" :key="doc.id" class="single">
-        <span class="created-at">{{ doc.createdAt.toDate() }}</span>
+    <div v-if="documents" class="messages" ref="messages">
+      <div v-for="doc in formattedDocuments" :key="doc.id" class="single">
+        <span class="created-at">{{ doc.createdAt }}</span>
         <span class="name">{{ doc.name }}</span>
         <span class="message">{{ doc.message }}</span>
       </div>
@@ -13,11 +13,38 @@
 
 <script>
 import getCollection from '../composables/getCollection'
+//installed date-fns so that we could formast the date and time stamp on the messages
+// a different way with the age being displayed instead of the time stamp
+
+import { formatDistanceToNow } from 'date-fns'
+import { computed, onUpdated, ref } from '@vue/runtime-core'
 export default {
   setup() {
     const { error, documents } = getCollection('messages')
 
-    return { error, documents }
+    //we fire a function that returns a value
+    const formattedDocuments = computed(() => {
+      if (documents.value) {
+        return documents.value.map((doc) => {
+          let time = formatDistanceToNow(doc.createdAt.toDate())
+          //we overwrite the createdAt property and use the new defined time variable
+          return { ...doc, createdAt: time }
+        })
+      }
+    })
+
+    //auto scroll to bottom of the messages
+    //by creating a template ref and calling it messages, then defining a constant by the same name
+    // in my script then created an onUpdated life cycle hook and then the hook takes the value of
+    // scrollHeight of the latest message that is being added to my chat window and it makes the
+    // last message the new top of the scroll .
+    const messages = ref(null)
+
+    onUpdated(() => {
+      messages.value.scrollTop = messages.value.scrollHeight
+    })
+
+    return { error, documents, formattedDocuments, messages }
   },
 }
 </script>
